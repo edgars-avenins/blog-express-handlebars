a4d88b9c24d2107db1338c83bbd0ad7ea9ca5951
Student <student@eda-bc-02.localdomain>
2019-10-26 12:12:34 +1300

    Basic blog from gitpages to express server structure set

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 
 // Import packages into our file
 const express = require('express')
+const hbs = require('express-handlebars')
 
 // Set up express server
 const server = express()
 
+//Use middleware
+
+server.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}))
+server.set('view engine', 'hbs')
 // Serve static files
+server.use(express.static('public'))
+
 
 // Set up routes
-// server.get('/', (req, res) => {
-//     res.send("Home Page")
-// })
+server.get('/', (req, res) => {
+    res.render('blog/indexBody', {})
+})
 
 // server.get('/blog', (req, res) => {
 //     res.send("Blog Page")
