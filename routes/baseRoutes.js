01410b3c8ba8651d1beb38a30c8bb5fbfe6d4cfe
Student <student@eda-bc-02.localdomain>
2019-10-26 14:51:00 +1300

    Blog succesfully remade with express server

--- routes/baseRoutes.js
+++ routes/baseRoutes.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const bool = {
+    passIf: true
+}
+
 router.get('/', (req, res) => {
-    res.render('blog/indexBody', {})
+    res.render('blog/indexBody', bool)
 })
 
 router.get('/contact', (req, res) => {
-    res.render('blog/contact', {})
+    res.render('blog/contact', bool)
 })
 
 router.get('/aboutme', (req, res) => {
-    res.render('blog/aboutMe', {})
+    res.render('blog/aboutMe', bool)
 })
 
 router.get('/featured', (req, res) => {
-    res.render('blog/featured', {})
+    res.render('blog/featured', bool)
 })
 
 
--- routes/sprintRoutes.js
+++ routes/sprintRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/central', (req, res) => {
-    res.render('blog/indexBody', {})
+    res.render('blog/sprintCentral', {})
+})
+
+const path = 'blog/sprints/'
+
+router.get('/:sprintID/:blogType', (req, res) => {
+    const id = req.params.sprintID
+    const type = req.params.blogType
+    console.log(req.params)
+    res.render(path + 's' + id + type, {})
 })
 
 
